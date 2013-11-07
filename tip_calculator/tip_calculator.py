43b6d4e2ee8448b4fa0f21912fbfb7caa3b97181
David Payne <dpayne162@gmail.com>
2013-11-06 22:09:15 -0600

    User Input Tip Calculator
    Version 2

--- tip_calculator/tip_calculator.py
+++ tip_calculator/tip_calculator.py
@@ -1,10 +1,13 @@
 # Tip calculator
 # Hard-Code
-meal_cost = 20.00
-tax_rate = .15 #15%
-tip_rate = .15
+#meal_cost = 20.00
+#tax_rate = .15 #15%
+#tip_rate = .15
 
-def tip_calculator(meal_cost, tax_rate, tip_rate):
+def tip_calculator():
+    meal_cost = float(raw_input("Please enter your pre tax and tip meal cost: "))
+    tax_rate = float(raw_input("Please provide your tax rate as a decimal: "))
+    tip_rate = float(raw_input("Please provide your desired tip rate as a decimal: "))
     print "The base cost of your meal was $%.2f" % meal_cost
     tax = meal_cost*tax_rate
     print "You need to pay $%.2f" % tax
@@ -13,4 +16,4 @@ def tip_calculator(meal_cost, tax_rate, tip_rate):
     total = meal_cost+tax+tip
     print "The grand total of your meal is $%.2f." % total
 
-tip_calculator(meal_cost, tax_rate, tip_rate)
+tip_calculator()
