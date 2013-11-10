ff4911959ebf511262a7d09de6b88cb132a8d67b
David Payne <dpayne162@gmail.com>
2013-11-09 18:00:17 -0600

    Tip Calculator with Exceptions

--- tip_calculator/tip_calculator.py
+++ tip_calculator/tip_calculator.py
@@ -1,35 +1,50 @@
 # Tip calculator
-# Hard-Code
-#meal_cost = 20.00
-#tax_rate = .15 #15%
-#tip_rate = .15
-#from sys import argv
-from optparse import OptionParser
+from sys import argv
 
-parser = OptionParser()
-parser.add_option("-m", "--first", dest="meal_cost", help="the meal cost", type = "float")
-parser.add_option("-x", "--second", dest="tax_rate", help="the tax rate", type = "float")
-parser.add_option("-t", "--third", dest="tip_rate", help="the tip rate", type = "float", default =".15")
+def calculate_rate(base, percentage):
+    return base * percentage
+ 
+def calculate_meal_costs(meal_base, tax_rate, tip_rate):
+    """
+    Calculates dollar amounts for tax, tip, and total meal cost
+    """
+    tax_value = calculate_rate(meal_base, tax_rate)
+    meal_with_tax = tax_value + meal_base
+    tip_value = calculate_rate(meal_with_tax, tip_rate)
+    total = meal_with_tax + tip_value
+    meal_info = dict(meal_base=meal_base,
+                    meal_with_tax=meal_with_tax,
+                    tip_value=tip_value,
+                    tax_value=tax_value,
+                    total = total)
+    return meal_info
 
-(options, args) = parser.parse_args()
-if not(options.meal_cost and options.tax_rate):
-	parser.error("You need to supply a value for meal_cost and tax_rate")
-
-def tip_calculator():
-    #argv
-    #meal_cost = float(argv[1])
-    #tax_rate = float(argv[2])
-    #tip_rate = float(argv[3])
-    #user_inputs
-    #meal_cost = float(raw_input("Please enter your pre tax and tip meal cost: "))
-    #tax_rate = float(raw_input("Please provide your tax rate as a decimal: "))
-    #tip_rate = float(raw_input("Please provide your desired tip rate as a decimal: "))
-    print "The base cost of your meal was $%.2f" % options.meal_cost
-    tax = options.meal_cost*options.tax_rate
-    print "You need to pay $%.2f" % tax
-    tip = options.tip_rate*(options.meal_cost+tax)
-    print "Tipping at a rate of %d%%, you should leave $%.2f for a tip." % (options.tip_rate*100, tip)
-    total = options.meal_cost+tax+tip
-    print "The grand total of your meal is $%.2f." % total
-
-tip_calculator()
+def main():
+    """
+    Takes in the cost of the meal, tax rate, and tip rate to produce the meal
+    costs. Prompts the user for valid inputs until given appropriate ones.
+    """
+    try:
+        meal_cost = float(argv[1])
+        tax_rate = float(argv[2])
+        tip_rate = float(argv[3])
+    except (ValueError, IndexError):
+        while True:
+            try:
+                print "You must provide a number for all inputs"
+                meal_cost = float(raw_input("Meal Cost: $"))
+                tax_rate = float(raw_input("Tax rate: "))
+                tip_rate = float(raw_input("Tip rate: "))
+                break
+            except ValueError:
+                pass
+    
+    meal_info = calculate_meal_costs(meal_cost, tax_rate, tip_rate)
+    print "The cost of your meal with tax is $%.2f" % meal_info['meal_with_tax']
+    print "Tipping at a rate of %d%%, you should leave $%.2f for a tip." % (tip_rate*100, meal_info['tip_value'])
+    print "The grand total for your meal is $%.2f." % meal_info['total']
+    if tip_rate < .15:
+        print "Only tipping %d%% tip? Little Cheap, eh?" % (tip_rate*100)
+    
+if __name__ == '__main__':
+    main()
