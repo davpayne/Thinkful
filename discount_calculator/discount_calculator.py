2b2b6c97ea5bf4c1e2095b98a1bc3352eacf548b
David Payne <dpayne162@gmail.com>
2013-11-11 21:56:57 -0600

    Final Discount Calculator and Tests

--- discount_calculator/discount_calculator.py
+++ discount_calculator/discount_calculator.py
@@ -1,5 +1,14 @@
 class DiscountCalculator(object):
-  def calculate(self, total, discount_amount, discount_type):
-    percent_discount = discount_amount/100.
-    discount = total * percent_discount
-    return discount
+	def calculate(self, total, discount_amount, discount_type):
+	    if discount_type == 'percent':
+	      if discount_amount > 100:
+	      	raise ValueError("Percentage discount cannot exceed 100%")
+	      percentage_discount = discount_amount / 100.
+	      discount = total * percentage_discount
+	    elif discount_type == 'absolute':
+	      if discount_amount > total:
+	      	raise ValueError("Discount cannot exceed order total.")
+	      discount = discount_amount
+	    else:
+	      raise ValueError("Invalid discount type: Options are 'percent' and 'absolute'.")
+	    return discount
