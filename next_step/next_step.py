b5832ca905996c5753722b3d74bd63708c9a6213
David Payne <dpayne162@gmail.com>
2013-11-20 06:58:23 -0600

    3 tests passed

--- next_step/next_step.py
+++ next_step/next_step.py
@@ -1,9 +1,29 @@
-class to_do(object):
-	projects = []
+class ToDo(object): 
+	contents = []
 	def __init__(self, name):
 		self.name = name
 
-	def add_project(self, pname):
-		projects.append(project(self.pname))
+	def __repr__(self):
+		values = {'name':self.name}
+		if len(self.contents) > 0:
+			values['contents'] = [str(item.name) for item in self.contents]
+		return str(values)
 
-	
+	def list_contents(self):
+		if len(self.contents) > 0:
+			list_contents = [str(item.name) for item in self.contents]
+		return list_contents
+
+	def add(self, pname):
+		self.contents.append(Project(pname))
+
+class Project(ToDo):
+	def __repr__(self):
+		return self.name
+"""
+if __name__ == '__main__':
+    t = ToDo('test1')
+    p = Project('Project1')
+    t.add(p)
+    print t
+    print t.list_contents()"""
--- next_step/nextstep_tests.py
+++ next_step/nextstep_tests.py
@@ -3,14 +3,19 @@ from next_step import *
 
 class NextStepTests(unittest.TestCase):
 	def create_to_do_test(self):
-		my_list = to_do('David')
+		my_list = ToDo('David')
 		self.assertEqual(my_list.name, 'David')
-		"""
-	def add_project_test(self):
-		my_list = to_do('David')
-		my_list.add_project('Test')
-		self.assertEqual(my_list.projects, ['Test'])
 	
+	def create_project_test(self):
+		my_project = Project('Test')
+		self.assertEqual(my_project.name, 'Test')
+		
+	def add_project_test(self):
+		my_list = ToDo('David')
+		my_list.add('Project')
+		self.assertEqual(my_list.list_contents(), ['Project'])
+
+	"""
 	def display_tasks_test(self):
 		my_list = to_do()
 		my_list.add_project('Learning')
