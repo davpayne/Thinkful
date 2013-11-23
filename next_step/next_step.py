0dfcbdb79e7ac1205e3f35a2fdbacb2586387eb6
David Payne <dpayne162@gmail.com>
2013-11-23 13:15:18 -0600

    Next Steps Task Manager

--- next_step/next_step.py
+++ next_step/next_step.py
@@ -1,29 +1,116 @@
-class ToDo(object): 
-	contents = []
+"""
+next_step is a task manager intended to help the user focus on 
+their next step only. It also tracks steps taken as a motivational tool.
+
+'You don't have to see the whole staircase, just take the first step'
+- Martin Luther King Jr.
+"""
+
+class Task(object):
+	"""
+	Base class for task manager. Has only basic attributes Used as base
+	for overall to do list and projects and is used as the task class.
+	"""
 	def __init__(self, name):
+		"""
+		Initializes the name of the object as well as the contents and
+		steps taken, only used by ToDo and Project functions. Contents
+		are the contained projects/tasks and steps taken tracks the progress
+		of each project and overall. 
+		"""
 		self.name = name
+		self.contents = []
+		self.steps_taken = 0
 
 	def __repr__(self):
+		"""
+		Returns the name of the object as well as the projects/tasks it contains
+		"""
 		values = {'name':self.name}
 		if len(self.contents) > 0:
 			values['contents'] = [str(item.name) for item in self.contents]
 		return str(values)
 
+class ToDo(Task):
+	""" 
+	Master list containing projects which then contain individual tasks
+	""" 
+	def __getitem__(self, index):
+		return self.contents[index] 
+
 	def list_contents(self):
+		"""
+		Converts list of class instances into a list of strings
+		"""
 		if len(self.contents) > 0:
 			list_contents = [str(item.name) for item in self.contents]
-		return list_contents
+			return list_contents
+		else:
+			return "Empty"
 
 	def add(self, pname):
-		self.contents.append(Project(pname))
+		"""
+		Adds a new project to the ToDo
+		"""
+		if type(pname) == str:
+			self.contents.append(Project(pname))
+		else:
+			self.contents.append(pname)
+
+	def next_steps(self):
+		"""
+		Lists out just the next step in each project
+		"""
+		next_steps = []
+		if len(self.contents) > 0:
+			next_steps = [self.contents[i].next_step() for i in 
+									range(len(self.contents))]
+			return next_steps
+		else:
+			return "Empty"
+
+	def total_steps_taken(self):
+		"""
+		Sums the total number of steps taken in each project.
+		"""
+		for item in range(len(self.contents)):
+			self.steps_taken +=self.contents[item].steps_taken
+		return self.steps_taken
+
+	def delete(self, arg):
+		"""
+		Removes a project/task either by its index or name
+		"""
+		if type(arg) == int:
+			i = arg
+		elif type(arg) == str:
+			i = self.list_contents().index(arg)
+		self.contents.pop(i)
 
 class Project(ToDo):
-	def __repr__(self):
-		return self.name
-"""
-if __name__ == '__main__':
-    t = ToDo('test1')
-    p = Project('Project1')
-    t.add(p)
-    print t
-    print t.list_contents()"""
+	"""
+	Contains the individual tasks
+	"""
+	def add(self, tname):
+		"""
+		Adds a task to the project
+		"""
+		if type(tname) == str:
+			self.contents.append(Task(tname)) 
+		else:
+			self.contents.append(tname)
+
+	def next_step(self):
+		"""
+		Returns what the next step in the project is. Done by list order
+		"""
+		return self.contents[0].name
+
+	def take_step(self):
+		"""
+		Checks off the next step in the project and adds to the steps counter
+		"""
+		if len(self.contents) > 0:
+			self.steps_taken += 1
+			self.contents.pop(0)
+			print "You have taken {} steps! Keep going!".format(self.steps_taken)
--- next_step/nextstep_tests.py
+++ next_step/nextstep_tests.py
@@ -3,22 +3,84 @@ from next_step import *
 
 class NextStepTests(unittest.TestCase):
 	def create_to_do_test(self):
-		my_list = ToDo('David')
-		self.assertEqual(my_list.name, 'David')
+		my_list = ToDo('To do list')
+		self.assertEqual(my_list.name, 'To do list')
 	
 	def create_project_test(self):
-		my_project = Project('Test')
-		self.assertEqual(my_project.name, 'Test')
+		my_project = Project('Project')
+		self.assertEqual(my_project.name, 'Project')
 		
 	def add_project_test(self):
 		my_list = ToDo('David')
 		my_list.add('Project')
 		self.assertEqual(my_list.list_contents(), ['Project'])
 
-	"""
-	def display_tasks_test(self):
-		my_list = to_do()
-		my_list.add_project('Learning')
-		my_list.add_task('Learning', 'Finish lesson 6')
-		my_list.add_task('Learning', 'Finish lesson 6')
-	"""
+	def access_project_test(self):
+		m_list = ToDo('To Do List')
+		m_list.add('Project')
+		self.assertEqual(type(m_list[0]), Project)
+
+	def create_tasks_test(self):
+		my_project = Project('Work')
+		my_project.add('Finish Deliverable')
+		self.assertEqual(my_project.list_contents(), ['Finish Deliverable'])
+
+	def add_project_to_list_test(self):
+		my_list = ToDo('Overall List')
+		my_list.add('Project')
+		my_list[0].add('Task')
+		self.assertEqual(my_list[0].list_contents(), ['Task'])
+
+	def next_project_step_test(self):
+		my_project = Project('Coding')
+		my_project.add('Sign up for Thinkful')
+		my_project.add('Complete Course')
+		self.assertEqual(my_project.next_step(), 'Sign up for Thinkful')
+
+	def all_next_steps_test(self):
+		my_list = ToDo('Life')
+		my_project = Project('Work')
+		my_other_project = Project('Coding')
+		my_list.add(my_project)
+		my_list.add(my_other_project)
+		my_list[0].add('Finish Deliverable')
+		my_list[0].add('Finish Objectives')
+		my_list[1].add('Complete Unit 2')
+		my_list[1].add('Complete Unit 3')
+		self.assertEqual(my_list.next_steps(), ['Finish Deliverable', 
+												'Complete Unit 2'])
+
+	def step_taken_test(self):
+		my_project = Project('Work')
+		my_project.add('First Step')
+		my_project.add('Second Step')
+		my_project.take_step()
+		self.assertEqual(my_project.list_contents(), ['Second Step'])
+		self.assertEqual(my_project.steps_taken, 1)
+
+	def total_steps_taken_test(self):
+		my_list = ToDo('Life')
+		my_project = Project('Work')
+		my_other_project = Project('Coding')
+		my_list.add(my_project)
+		my_list.add(my_other_project)
+		my_list[0].add('Finish Deliverable')
+		my_list[0].add('Finish Objectives')
+		my_list[1].add('Complete Unit 2')
+		my_list[1].add('Complete Unit 3')
+		my_project.take_step()
+		my_other_project.take_step()
+		self.assertEqual(my_project.steps_taken, 1)
+		self.assertEqual(my_list.total_steps_taken(), 2)
+
+	def delete_projects_test(self):
+		my_list = ToDo('Life')
+		my_project = Project('Work')
+		my_other_project = Project('Coding')
+		another_project = Project('Class')
+		my_list.add(my_project)
+		my_list.add(my_other_project)
+		my_list.add(another_project)
+		my_list.delete(0)
+		my_list.delete('Class')
+		self.assertEqual(my_list.list_contents(), ['Coding'])
